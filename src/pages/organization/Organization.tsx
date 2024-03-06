0d6eceaed3ca87c493a28d7b958b69fe1355ba30
Nan <sreynan.soeun@anbschool.org>
2024-03-06 18:16:38 +0700

    change: change structure of create new organization screen

--- src/pages/organization/Organization.tsx
+++ src/pages/organization/Organization.tsx
@@ -1,30 +1,33 @@
+import CP from "@/components";
 import Typography from "@/components/typography";
 import { Container } from "@mui/material";
 import Box from "@mui/material/Box";
 import CreateOrgStepper from "@/components/stepper/CreateOrgStepper";
-import TextField from "@/components/textField";
 import MapComponent from "@/components/map/Map";
+import Stack from "@mui/material/Stack";
+
 const Organization = () => {
   return (
-    <Container sx={{ width: "100%", paddingTop: 6 }}>
+    <Container sx={{ paddingTop: 6 }}>
       <CreateOrgStepper />
 
       <MapComponent />
-      <Box>
+
+      <Stack gap={2}>
         <Typography>Organization Information</Typography>
         <Box>
-          <TextField label="Name" />
+          <CP.Input label="Name" />
         </Box>
         <Box>
-          <TextField label="Organization Name" />
+          <CP.Input label="Organization Name" />
         </Box>
         <Box>
-          <TextField label="Address Line" />
+          <CP.Input label="Address Line" />
         </Box>
         <Box>
-          <TextField label="Currency " />
+          <CP.Input label="Currency " />
         </Box>
-      </Box>
+      </Stack>
     </Container>
   );
 };
