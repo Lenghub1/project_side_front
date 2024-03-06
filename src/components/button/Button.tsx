d8e6875a4549e20f4a8625aa19753ef058fc43c0
phirom02 <phiromkhim02@gmail.com>
2024-03-06 14:47:16 +0700

    style: Remove Button wrapper as it affect the component spacing.

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,7 +1,6 @@
 import MuiButton, { ButtonProps as MuiButtonProps } from "@mui/material/Button";
 import { ReactNode } from "react";
 
-import CP from "..";
 import { ColorPalette } from "@/@type/common";
 
 /**
@@ -14,11 +13,9 @@ export interface ButtonProps extends MuiButtonProps {
 
 const Button = ({ children, ...props }: ButtonProps) => {
   return (
-    <CP.Styled.ButtonWrapper>
-      <MuiButton variant="contained" {...props}>
-        {children}
-      </MuiButton>
-    </CP.Styled.ButtonWrapper>
+    <MuiButton variant="contained" {...props}>
+      {children}
+    </MuiButton>
   );
 };
 
