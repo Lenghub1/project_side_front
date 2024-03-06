522ed2b1a384ba5425dc213057fde305a2a16709
Nan <sreynan.soeun@anbschool.org>
2024-03-06 14:18:17 +0700

    fix: fix husky error for commitlint

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,12 +1,6 @@
 module.exports = {
-  /*
-   * Resolve and load @commitlint/config-conventional from node_modules.
-   * Referenced packages must be installed
-   */
   extends: ["@commitlint/config-conventional"],
-  /*
-   * Any rules defined here will override rules from @commitlint/config-conventional
-   */
+
   rules: {
     "type-enum": [
       2,
@@ -23,8 +17,8 @@ module.exports = {
         "refactor",
         "revert",
         "style",
-        "test"
-      ]
-    ]
-  }
+        "test",
+      ],
+    ],
+  },
 };
