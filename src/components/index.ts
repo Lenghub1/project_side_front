c1f7ab124713c69a88fc3e9f85f68b5bd35c2168
Teb Yuma <yuma.teb@anbschool.org>
2024-03-07 15:02:26 +0700

    feat: forgetpassword, OTP and reset password screen

--- src/components/index.ts
+++ src/components/index.ts
@@ -7,6 +7,7 @@ import Checkbox from "./checkbox";
 import Modal from "./modal";
 import Typography from "./typography";
 import PhonePrefix from "./phonePrefix";
+import InputBox from "./inputbox/Inputbox";
 import Container from "./container";
 import SnackbarMessage from "./showMessage";
 
@@ -24,6 +25,7 @@ const CP = {
   Input,
   Typography,
   PhonePrefix,
+  InputBox,
   Container,
   SnackbarMessage,
 };
