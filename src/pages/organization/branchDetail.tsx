e77831bda6f6c608f2e035981387c6b8db774bd3
LENG <hourtourleng.hort@anbschool.org>
2024-03-07 17:55:47 +0700

    feat: overView organization and branch

--- src/pages/organization/branchDetail.tsx
+++ src/pages/organization/branchDetail.tsx
@@ -11,7 +11,7 @@ interface BranchDetailCardState {
 }
 
 export const BranchDetailCard: React.FC<BranchDetailCardProps> = ({
-  branchName
+  branchName,
 }) => {
   const [expanded, setExpanded] = useState<boolean>(false);
 
@@ -20,17 +20,13 @@ export const BranchDetailCard: React.FC<BranchDetailCardProps> = ({
   };
 
   return (
-    <CP.Card
-      border="none"
-      borderBottom="0.5px solid"
-      borderRadius="0"
-      padding="20px"
-      onClick={toggleExpansion}
-    >
+    <CP.Card padding="20px" onClick={toggleExpansion} width="100%">
       <CP.Styled.Flex direction="column" items="flex-start">
         <CP.Styled.Div>
           <CP.Styled.Flex justify="space-between">
-            <CP.Typography variant="h5">{branchName}</CP.Typography>
+            <CP.Typography variant="subtitle1" fontWeight={500}>
+              {branchName}
+            </CP.Typography>
             <ExpandMoreIcon />
           </CP.Styled.Flex>
         </CP.Styled.Div>
@@ -45,7 +41,7 @@ export const BranchDetailCard: React.FC<BranchDetailCardProps> = ({
                   "url('https://cdn.britannica.com/37/245037-050-79129D52/world-map-continents-oceans.jpg')",
                 backgroundSize: "cover",
                 backgroundPosition: "center",
-                marginTop: "20px"
+                marginTop: "20px",
               }}
               alt=""
             />
@@ -64,7 +60,12 @@ export const BranchDetailCard: React.FC<BranchDetailCardProps> = ({
                 </CP.Typography>
                 <CP.Typography color={"gray"}> 6 Employee</CP.Typography>
               </CP.Styled.Flex>
-              <CP.Styled.Flex gap="10px" justify="flex-end" items="flex-end">
+              <CP.Styled.Flex
+                gap="10px"
+                justify="flex-end"
+                items="flex-end"
+                overflow="unset"
+              >
                 <CP.Button variant="text">Archieve</CP.Button>
                 <CP.Button style={{ borderRadius: "20px" }}>Modify</CP.Button>
               </CP.Styled.Flex>
--- src/pages/organization/overviewOrgnization.tsx
+++ src/pages/organization/overviewOrgnization.tsx
@@ -4,6 +4,12 @@ import theme from "@/theme/ligthTheme";
 
 import { BranchDetailCard } from "./branchDetail"; // Importing the BranchDetailCard component
 import Container from "@mui/material/Container"; // Import Container from Material-UI
+import styled from "styled-components";
+import { Divider } from "@mui/material";
+
+const Flex = styled(CP.Styled.Flex)`
+  overflow: unset;
+`;
 
 const OverviewOrganization = () => {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 456);
@@ -23,114 +29,95 @@ const OverviewOrganization = () => {
   return (
     <Container maxWidth="lg">
       <CP.Styled.Wrapper padding="20px" overflow="auto" width="auto">
-        <CP.Styled.Div>
-          <CP.Styled.Flex direction="column" gap="10px">
-            <CP.Card
-              padding="0"
-              border="none"
-              borderBottom="0.5px solid"
-              borderRadius="none"
-            >
-              <CP.Styled.Flex gap={isMobile ? "20px" : "50px"}>
-                <CP.Button
-                  variant="text"
-                  sx={{
-                    padding: "10px",
-                    color: `${theme.palette.primary}`,
-                    borderBottom: "2px solid",
-                    borderRadius: "0"
-                  }}
-                >
-                  OVERVIEW
-                </CP.Button>
-                <CP.Button
-                  variant="text"
-                  sx={{
-                    padding: "10px",
-                    color: `${theme.palette.text.secondary}`
-                  }}
-                >
-                  PERMISSION
-                </CP.Button>
-                <CP.Button
-                  variant="text"
-                  sx={{
-                    padding: "10px",
-                    color: `${theme.palette.text.secondary}`
-                  }}
-                >
-                  ADVANCES
-                </CP.Button>
+        <Flex direction="column" gap="10px">
+          <CP.Card padding="0" width="100%">
+            <CP.Styled.Flex gap={isMobile ? "20px" : "50px"}>
+              <CP.Button
+                variant="text"
+                sx={{
+                  padding: "10px",
+                  color: `${theme.palette.primary}`,
+                  borderBottom: "2px solid",
+                  borderRadius: "0",
+                }}
+              >
+                OVERVIEW
+              </CP.Button>
+              <CP.Button
+                variant="text"
+                sx={{
+                  padding: "10px",
+                  color: `${theme.palette.text.secondary}`,
+                }}
+              >
+                PERMISSION
+              </CP.Button>
+              <CP.Button
+                variant="text"
+                sx={{
+                  padding: "10px",
+                  color: `${theme.palette.text.secondary}`,
+                }}
+              >
+                ADVANCES
+              </CP.Button>
+            </CP.Styled.Flex>
+          </CP.Card>
+
+          <Flex
+            direction="column"
+            items="flex-start"
+            gap="10px"
+            overflow="unset"
+          >
+            <CP.Typography sx={{ marginTop: "20px" }} variant="h6">
+              Detail
+            </CP.Typography>
+            <CP.Card width="100%">
+              <CP.Styled.Flex direction="column" gap="10px">
+                <CP.Styled.Flex direction="column" items="flex-start">
+                  <CP.Typography>Name</CP.Typography>
+                  <CP.Typography variant="subtitle1">Sandbox</CP.Typography>
+                </CP.Styled.Flex>
+                <Divider sx={{ width: "100%" }} />
+                <CP.Styled.Flex direction="column" items="flex-start">
+                  <CP.Typography>Number of Employee</CP.Typography>
+                  <CP.Typography variant="subtitle1">56</CP.Typography>
+                </CP.Styled.Flex>
+                <Divider sx={{ width: "100%" }} />
+                <CP.Styled.Flex direction="column" items="flex-start">
+                  <CP.Typography>Currency</CP.Typography>
+                  <CP.Typography variant="subtitle1">Riel</CP.Typography>
+                </CP.Styled.Flex>
+                <Divider sx={{ width: "100%" }} />
+                <CP.Styled.Flex direction="column" items="flex-start">
+                  <CP.Typography>Location</CP.Typography>
+                  <CP.Typography variant="subtitle1">
+                    abc, 123, Phnom Penh, Cambodia
+                  </CP.Typography>
+                </CP.Styled.Flex>
               </CP.Styled.Flex>
             </CP.Card>
-            <CP.Styled.Div>
-              <CP.Styled.Flex direction="column" items="flex-start" gap="10px">
-                <CP.Typography sx={{ marginTop: "20px" }} variant="h6">
-                  Detail
-                </CP.Typography>
-                <CP.Card border="none">
-                  <CP.Styled.Flex direction="column" gap="10px">
-                    <CP.Styled.Flex
-                      direction="column"
-                      items="flex-start"
-                      style={{ borderBottom: "0.5px solid" }}
-                    >
-                      <CP.Typography>Name</CP.Typography>
-                      <CP.Typography variant="subtitle1">Sandbox</CP.Typography>
-                    </CP.Styled.Flex>
-                    <CP.Styled.Flex
-                      direction="column"
-                      items="flex-start"
-                      style={{ borderBottom: "0.5px solid" }}
-                    >
-                      <CP.Typography>Number of Employee</CP.Typography>
-                      <CP.Typography variant="subtitle1">56</CP.Typography>
-                    </CP.Styled.Flex>
-                    <CP.Styled.Flex
-                      direction="column"
-                      items="flex-start"
-                      style={{ borderBottom: "0.5px solid" }}
-                    >
-                      <CP.Typography>Currency</CP.Typography>
-                      <CP.Typography variant="subtitle1">Riel</CP.Typography>
-                    </CP.Styled.Flex>
-                    <CP.Styled.Flex
-                      direction="column"
-                      items="flex-start"
-                      style={{ borderBottom: "0.5px solid" }}
-                    >
-                      <CP.Typography>Location</CP.Typography>
-                      <CP.Typography variant="subtitle1">
-                        abc, 123, Phnom Penh, Cambodia
-                      </CP.Typography>
-                    </CP.Styled.Flex>
-                  </CP.Styled.Flex>
-                </CP.Card>
-              </CP.Styled.Flex>
-            </CP.Styled.Div>
-            <CP.Styled.Div>
-              <CP.Styled.Flex direction="column" items="flex-start" gap="10px">
-                <CP.Styled.Flex
-                  items="center"
-                  justify="space-between"
-                  margin="10px 0"
-                >
-                  <CP.Typography sx={{ textAlign: "center" }} variant="h6">
-                    BRANCHES(2)
-                  </CP.Typography>
-                  <CP.Button>Create</CP.Button>
-                </CP.Styled.Flex>
+          </Flex>
 
-                <CP.Card border="none">
-                  <CP.Styled.Flex direction="column">
-                    <BranchDetailCard branchName="Branch1" />
-                    <BranchDetailCard branchName="Branch2" />
-                  </CP.Styled.Flex>
-                </CP.Card>
-              </CP.Styled.Flex>
-            </CP.Styled.Div>
-          </CP.Styled.Flex>
-        </CP.Styled.Div>
+          <Flex direction="column" items="flex-start" gap="10px">
+            <CP.Styled.Flex
+              items="center"
+              justify="space-between"
+              margin="10px 0"
+            >
+              <CP.Typography sx={{ textAlign: "center" }} variant="h6">
+                BRANCHES(2)
+              </CP.Typography>
+              <CP.Button>Create</CP.Button>
+            </CP.Styled.Flex>
+
+            <Flex direction="column" gap="20px">
+              <BranchDetailCard branchName="Branch1" />
+              <BranchDetailCard branchName="Branch2" />
+            </Flex>
+          </Flex>
+        </Flex>
       </CP.Styled.Wrapper>
     </Container>
   );
