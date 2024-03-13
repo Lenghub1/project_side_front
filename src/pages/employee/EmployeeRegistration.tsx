b69db3934385538ac57efad9abe88338fc4e2723
Nan <sreynan.soeun@anbschool.org>
2024-03-13 16:22:34 +0700

    add: handle error from fetching the data in the employeeRegistration page

--- src/pages/employee/EmployeeRegistration.tsx
+++ src/pages/employee/EmployeeRegistration.tsx
@@ -3,37 +3,63 @@ import EnhancedTable from "@/components/table/Table";
 import { Container } from "@mui/material";
 import { getAllPendingEmployees } from "@/api/employee";
 import { handleApiRequest } from "@/api";
+import { socket } from "@/socket";
+
+const userId = "d5e2b24b-7c77-480f-ad24-c79c786179cc";
 
 const EmployeeRegistration = () => {
-  const [data, setData] = React.useState<any>([]);
+  const [data, setData] = React.useState<Object | any>([]);
+  const [error, setError] = React.useState<Object | any>([]);
 
   const newPendingEmployees = async () => {
     const [response, error] = await handleApiRequest(() =>
-      getAllPendingEmployees("30ed163a-f86f-4b6d-8a9e-eb4263e5a9de")
+      getAllPendingEmployees()
     );
     if (response) {
       console.log(response);
       setData(response);
+      setError(undefined);
     } else {
       console.log(error);
+      setError(error);
     }
   };
 
   React.useEffect(() => {
     newPendingEmployees();
-  }, []);
+    console.log(userId);
+    socket.on(`on-updates-${userId}`, (data) => {
+      console.log(data);
+    });
+  }, [socket]);
 
-  return (
-    <Container>
-      <EnhancedTable
-        onRequestSort={(_, property) => console.log(property)}
-        order="asc"
-        rows={data}
-        orderBy="name"
-        rowCount={data.length}
-      />
-    </Container>
-  );
+  if (error) {
+    if (error.response?.status === 404) {
+      return (
+        <Container sx={{ color: (theme) => theme.palette.text.primary }}>
+          <h1>There's no pending request</h1>
+        </Container>
+      );
+    } else {
+      return (
+        <Container sx={{ color: (theme) => theme.palette.text.primary }}>
+          <h1>Something went wrong</h1>
+        </Container>
+      );
+    }
+  } else {
+    return (
+      <Container>
+        <EnhancedTable
+          onRequestSort={(_, property) => console.log(property)}
+          order="asc"
+          rows={data}
+          orderBy="name"
+          rowCount={data.length}
+        />
+      </Container>
+    );
+  }
 };
 
 export default EmployeeRegistration;
