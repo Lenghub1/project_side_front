1e505951b9dd4103f0daa32a8d2bb709998a40a1
kimsang <kimsang.mok@anbschool.org>
2024-03-13 18:42:37 +0700

    add: create UI for company look-up screen

--- src/pages/CompanyLookup/CompanyLookup.tsx
+++ src/pages/CompanyLookup/CompanyLookup.tsx
@@ -33,46 +33,78 @@ const CompanyLookup = () => {
   }
 
   async function login(method: string, data: any): Promise<void> {
-    const [response, error] = await handleApiRequest(() =>
-      authApi.testLogin(method, data)
-    );
+    // const [response, error] = await handleApiRequest(() =>
+    //   authApi.testLogin(method, data)
+    // );
   }
+
+  function handleOnSubmit() {
+    if (!companyCode.value) {
+      companyCode.setTouched(true);
+    }
+  }
+
+  const Title = ({ children }: any) => (
+    <CP.Typography
+      variant="h4"
+      style={{
+        marginBottom: "2rem",
+        fontWeight: "semibold",
+        textAlign: "start",
+      }}
+    >
+      {children}
+    </CP.Typography>
+  );
 
   return (
     <CP.Styled.Wrapper height="100vh" padding="0">
       <Flex height="100%">
         <CP.Styled.Div padding="0 1rem">
           <CP.Styled.Form>
-            <Flex items="flex-start" padding="0 3rem" direction="column">
-              <CP.Typography
-                variant="h4"
-                style={{
-                  marginBottom: "2rem",
-                  fontWeight: "semibold",
-                  textAlign: "start",
-                }}
-              >
-                Login
-              </CP.Typography>
+            <Flex
+              items="flex-start"
+              padding="0 3rem"
+              direction="column"
+              gap="1rem"
+            >
+              <CP.Styled.Div>
+                <Title>Search Company</Title>
+                <Flex direction="column" gap="1.5rem" items="flex-start">
+                  <CP.Typography variant="body1">
+                    You can search for a company by typing in the correct
+                    company code.
+                  </CP.Typography>
 
-              <Flex direction="column" gap="1.5rem">
-                <Divider></Divider>
-                <Flex gap="1rem">
-                  <CP.Button
-                    type="submit"
-                    onClick={() => console.log("Submit")}
-                    fullWidth
-                  >
-                    Login
-                  </CP.Button>
+                  <CP.Input
+                    label="Company Code"
+                    required
+                    value={companyCode.value}
+                    onChange={companyCode.onChange}
+                    onBlur={companyCode.onBlur}
+                    error={!!companyCode.error}
+                    helperText={<companyCode.HelperText />}
+                  />
+
+                  <Flex gap="1rem" justify="flex-end">
+                    <CP.Button variant="text">Cancel</CP.Button>
+                    <CP.Button type="submit" onClick={handleOnSubmit}>
+                      Login
+                    </CP.Button>
+                  </Flex>
                 </Flex>
-
-                <CP.Typography variant="subtitle2">
-                  By signing up, you agree to our{" "}
-                  <NavLink to="#">Terms of Service</NavLink> &{" "}
-                  <NavLink to="#">Privacy Policy</NavLink>
+              </CP.Styled.Div>
+              <CP.Styled.Div>
+                <Title>Join by invitation</Title>
+                <CP.Typography variant="body1">
+                  Check your inbox (and spam folder) for a Riem invite from your
+                  manager
                 </CP.Typography>
-              </Flex>
+                <CP.Typography variant="body1">
+                  If your manager hasn’t invited you yet, please contact and
+                  provide them your email or phone number to invite you
+                </CP.Typography>
+              </CP.Styled.Div>
             </Flex>
             <Flex direction="column" margin="1rem 0 0" gap="1rem">
               <Divider></Divider>
