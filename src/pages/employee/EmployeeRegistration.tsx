3672995efecdd5c44b4fc47f168381f3ce12d530
Nan <sreynan.soeun@anbschool.org>
2024-03-14 14:59:35 +0700

    change: use the actionCell in the EmployeeRegistration Table

--- src/pages/employee/EmployeeRegistration.tsx
+++ src/pages/employee/EmployeeRegistration.tsx
@@ -1,47 +1,76 @@
 import React from "react";
 import CP from "@/components";
+import CP from "@/components";
 import EnhancedTable from "@/components/table/Table";
+import Container from "@mui/material/Container";
 import Button from "@/components/button";
 import Badge from "@mui/material/Badge";
+import Box from "@mui/material/Box";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import { getAllPendingEmployees } from "@/api/employee";
+import { handleApiRequest } from "@/api";
+import { socket } from "@/socket";
 import { HeadCell } from "@/components/table/TableHead";
 import { Employement } from "@/utils/interfaces/Employment";
 import {
   handleAcceptEmployee,
   handleRejectEmployee,
 } from "@/utils/employee.util";
-import useFetch from "@/hooks/useFetch";
-import Error from "../error/Error";
-import { useRecoilState } from "recoil";
-import { allEmployeesData } from "@/store/employee";
+
+const userId = "d5e2b24b-7c77-480f-ad24-c79c786179cc";
 
 const RenderActionCell = (row: Employement) => {
   const { id } = row;
   return (
-    <CP.Styled.Flex gap="8px" justify="flex-start">
+    <CP.Styled.Flex>
       <Button onClick={() => handleAcceptEmployee(id)}>Accept</Button>
-      <Button color="accent" onClick={() => handleRejectEmployee(id)}>
-        Reject
-      </Button>
+      <Button onClick={() => handleRejectEmployee(id)}>Reject</Button>
     </CP.Styled.Flex>
   );
 };
 
 const EmployeeRegistration = () => {
-  const [allEmployee, setAllEmployee] = useRecoilState(allEmployeesData);
-  const { data, error } = useFetch(getAllPendingEmployees);
+  const [data, setData] = React.useState<Object | any>([]);
+  const [error, setError] = React.useState<Object | any>([]);
   const [notifiactionCount, setNotifiactionCount] = React.useState<number>(0);
 
+  const newPendingEmployees = async () => {
+    const [response, error] = await handleApiRequest(() =>
+      getAllPendingEmployees("1ca2a528-72c9-4cb8-8823-4d26cfcdd598")
+    );
+    if (response) {
+      console.log(response);
+      setData(response);
+      setError(undefined);
+    } else {
+      console.log(error);
+      setError(error);
+    }
+  };
+
+  React.useEffect(() => {
+    newPendingEmployees();
+    console.log(userId);
+  }, []);
+
   if (error) {
-    return <Error status={error.status_code} />;
-  }
-  setAllEmployee(data);
-  console.log(allEmployee);
-  return (
-    <CP.Container>
-      <CP.Container>
-        <CP.Styled.Flex justify="flex-end">
+    if (error.response?.status === 404) {
+      return (
+        <Container sx={{ color: (theme) => theme.palette.text.primary }}>
+          <h1>There's no pending request</h1>
+        </Container>
+      );
+    } else {
+      return (
+        <Container sx={{ color: (theme) => theme.palette.text.primary }}>
+          <h1>Something went wrong</h1>
+        </Container>
+      );
+    }
+  } else {
+    return (
+      <Container>
+        <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
           <Badge
             badgeContent={notifiactionCount}
             sx={{
@@ -52,20 +81,19 @@ const EmployeeRegistration = () => {
           >
             <NotificationsIcon />
           </Badge>
-        </CP.Styled.Flex>
-      </CP.Container>
-
-      <EnhancedTable<Employement>
-        headCells={headCells}
-        order="asc"
-        rows={data || []}
-        orderBy="name"
-        rowCount={data?.length || 0}
-        tableName="Employee Registrations"
-        actionCell={RenderActionCell}
-      />
-    </CP.Container>
-  );
+        </Box>
+        <EnhancedTable<Employement>
+          headCells={headCells}
+          order="asc"
+          rows={data}
+          orderBy="name"
+          rowCount={data.length}
+          tableName="Employee Registrations"
+          actionCell={RenderActionCell}
+        />
+      </Container>
+    );
+  }
 };
 
 export default EmployeeRegistration;
@@ -76,7 +104,6 @@ const headCells: HeadCell<Employement>[] = [
     numeric: false,
     disablePadding: false,
     label: "Emplyee Information",
-    filterable: true,
   },
   {
     id: "position",
@@ -89,13 +116,11 @@ const headCells: HeadCell<Employement>[] = [
     numeric: false,
     disablePadding: false,
     label: "Priviledges",
-    filterable: true,
   },
   {
     id: "status",
     numeric: false,
     disablePadding: false,
     label: "Status",
-    filterable: true,
   },
 ];
