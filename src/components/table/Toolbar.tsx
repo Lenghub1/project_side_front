6d8f474bff5eac098548ee0ed42ce4bbaaa1f4e3
Nan <sreynan.soeun@anbschool.org>
2024-03-14 14:45:46 +0700

    add: separate the toolbar component from table

---
+++ src/components/table/Toolbar.tsx
@@ -0,0 +1,37 @@
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+import FilterListIcon from "@mui/icons-material/FilterList";
+
+interface TableToolbarProp {
+  name: string;
+}
+
+const EnhancedTableToolbar: React.FC<TableToolbarProp> = ({ name }) => {
+  return (
+    <Toolbar
+      sx={{
+        pl: { sm: 2 },
+        pr: { xs: 1, sm: 1 },
+      }}
+    >
+      <Typography
+        sx={{ flex: "1 1 100%" }}
+        variant="h6"
+        id="tableTitle"
+        component="div"
+      >
+        {name}
+      </Typography>
+
+      <Tooltip title="Filter list">
+        <IconButton>
+          <FilterListIcon />
+        </IconButton>
+      </Tooltip>
+    </Toolbar>
+  );
+};
+
+export default EnhancedTableToolbar;
