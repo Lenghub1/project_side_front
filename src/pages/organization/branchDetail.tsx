f1324b44c13d32a291cd4c414899d05841f5a546
LENG <hourtourleng.hort@anbschool.org>
2024-03-14 22:32:31 +0700

    refactor: fixing rotation of icon for extanding information of each branch

--- src/pages/organization/branchDetail.tsx
+++ src/pages/organization/branchDetail.tsx
@@ -8,26 +8,50 @@ interface BranchDetailCardProps {
 
 interface BranchDetailCardState {
   expanded: boolean;
+  iconHovered: boolean;
 }
 
 export const BranchDetailCard: React.FC<BranchDetailCardProps> = ({
   branchName,
 }) => {
   const [expanded, setExpanded] = useState<boolean>(false);
-
+  const [iconHovered, setIconHovered] = useState<boolean>(false);
+  const [isActive, setIsActive] = useState<boolean>(false);
   const toggleExpansion = () => {
     setExpanded(!expanded);
+    setIsActive(!isActive);
+  };
+
+  const handleIconHover = () => {
+    setIconHovered(true);
+  };
+
+  const handleIconBlur = () => {
+    setIconHovered(false);
   };
 
   return (
-    <CP.Card padding="20px" onClick={toggleExpansion} width="100%">
+    <CP.Card padding="20px" width="100%">
       <CP.Styled.Flex direction="column" items="flex-start">
         <CP.Styled.Div>
           <CP.Styled.Flex justify="space-between">
             <CP.Typography variant="subtitle1" fontWeight={500}>
               {branchName}
             </CP.Typography>
-            <ExpandMoreIcon />
+            <ExpandMoreIcon
+              onMouseDown={handleIconHover}
+              onMouseUp={handleIconBlur}
+              onTouchStart={handleIconHover}
+              onTouchEnd={handleIconBlur}
+              style={{
+                color: iconHovered ? "#007bff" : "inherit",
+                transform:
+                  iconHovered || isActive ? "rotate(180deg)" : "rotate(0deg)",
+                transition: "transform 0.3s, color 0.3s",
+                cursor: "pointer",
+              }}
+              onClick={toggleExpansion}
+            />
           </CP.Styled.Flex>
         </CP.Styled.Div>
 
@@ -66,7 +90,7 @@ export const BranchDetailCard: React.FC<BranchDetailCardProps> = ({
                 items="flex-end"
                 overflow="unset"
               >
-                <CP.Button variant="text">Archieve</CP.Button>
+                <CP.Button variant="text">Archive</CP.Button>
                 <CP.Button style={{ borderRadius: "20px" }}>Modify</CP.Button>
               </CP.Styled.Flex>
             </CP.Styled.Div>
