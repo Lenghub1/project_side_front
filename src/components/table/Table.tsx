3fdfdaefac51fc0baec218e3d6b6cdd5cfaaaf95
Nan <sreynan.soeun@anbschool.org>
2024-03-14 14:49:46 +0700

    refactor: refactor the table component to be more dynamic

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -6,119 +6,70 @@ import TableContainer from "@mui/material/TableContainer";
 import TablePagination from "@mui/material/TablePagination";
 import { useNavigate } from "react-router-dom";
 import TableRow from "@mui/material/TableRow";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
-import IconButton from "@mui/material/IconButton";
-import Tooltip from "@mui/material/Tooltip";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Switch from "@mui/material/Switch";
-import FilterListIcon from "@mui/icons-material/FilterList";
-import Box from "@mui/material/Box";
-import { Avatar, Container } from "@mui/material";
-import { Employement } from "@/utils/interfaces/Employment";
-import CP from "..";
+import Container from "@mui/material/Container";
+import EnhancedTableToolbar from "./Toolbar";
 import EnhancedTableHead, { HeadCell } from "./TableHead";
-import { socket } from "src/socket";
-import { updateEmployee } from "@/api/employee";
-
-const orgId = "30ed163a-f86f-4b6d-8a9e-eb4263e5a9de";
-
-// socket.emit("join", `org-30ed163a-f86f-4b6d-8a9e-eb4263e5a9de`);
-// socket.on("update", (r) => {
-//   console.log(r);
-// });
-interface Data extends Employement {
-  action: string;
-}
-export interface EmploymentWithAction extends Employement {
-  action: string;
-}
-
-function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
+import { getComparator, stableSort } from "@/utils/table.util";
 
 type Order = "asc" | "desc";
 
-function getComparator<Key extends keyof any>(
-  order: Order,
-  orderBy: Key
-): (a: { [key in Key]: string }, b: { [key in Key]: string }) => number {
-  return order === "desc"
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-function stableSort<T>(array: T[], comparator: (a: T, b: T) => number) {
-  const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) {
-      return order;
-    }
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map((el) => el[0]);
-}
-
 interface EnhancedTableProps<T> {
+  headCells: HeadCell<T>[];
   rows: T[];
-  onRequestSort: (property: keyof T, event: React.MouseEvent<unknown>) => void;
   order: Order;
   orderBy: keyof T;
   rowCount: number;
+  tableName: string;
+  actionCell?: (row: T) => React.ReactNode;
 }
 
-function EnhancedTableToolbar() {
-  return (
-    <Toolbar
-      sx={{
-        pl: { sm: 2 },
-        pr: { xs: 1, sm: 1 },
-      }}
-    >
-      <Typography
-        sx={{ flex: "1 1 100%" }}
-        variant="h6"
-        id="tableTitle"
-        component="div"
-      >
-        Employee Registrations
-      </Typography>
-
-      <Tooltip title="Filter list">
-        <IconButton>
-          <FilterListIcon />
-        </IconButton>
-      </Tooltip>
-    </Toolbar>
-  );
-}
-
-// ... (import statements remain unchanged)
-
-const EnhancedTable: React.FC<EnhancedTableProps<EmploymentWithAction>> = ({
+/**
+ * EnhancedTable Component
+ *
+ * This component displays an enhanced table with sorting, pagination, dense padding, and optional action cells.
+ *
+ * Props:
+ *   - headCells: An array of objects defining the table header cells.
+ *   - rows: An array of data objects to be displayed in the table rows.
+ *   - order: The current sorting order ('asc' for ascending, 'desc' for descending).
+ *   - orderBy: The currently sorted column key.
+ *   - rowCount: The total number of rows.
+ *   - tableName: The name of the table.
+ *   - actionCell: An optional function that returns ReactNode to be rendered in an additional action cell for each row.
+ *
+ * @param {Array} headCells - An array of objects defining the table header cells.
+ * @param {Array} rows - An array of data objects to be displayed in the table rows.
+ * @param {string} order - The current sorting order ('asc' for ascending, 'desc' for descending).
+ * @param {string} orderBy - The currently sorted column key.
+ * @param {number} rowCount - The total number of rows.
+ * @param {string} tableName - The name of the table.
+ * @param {Function} [actionCell] - An optional function that returns ReactNode to be rendered in an additional action cell for each row.
+ *
+ * @returns {JSX.Element} React component
+ */
+function EnhancedTable<T>({
+  headCells,
   rows,
-}) => {
-  const [order, setOrder] = React.useState<Order>("asc");
-  const [orderBy, setOrderBy] = React.useState<keyof Data>("name");
+  order,
+  orderBy,
+  rowCount,
+  tableName,
+  actionCell,
+}: EnhancedTableProps<T>) {
   const [page, setPage] = React.useState(0);
   const [dense, setDense] = React.useState(false);
   const [employeeDetail, setEmployeeDetail] = useRecoilState(employementDetail);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [currentOrder, setCurrentOrder] = React.useState<Order>(order);
+  const [currentOrderBy, setCurrentOrderBy] = React.useState<keyof T>(orderBy);
 
-  console.log(rows);
-  const handleRequestSort = (property: keyof Data) => {
-    const isAsc = orderBy === property && order === "asc";
-    setOrder(isAsc ? "desc" : "asc");
-    setOrderBy(property);
+  const handleRequestSort = (property: keyof T) => {
+    const isAsc = currentOrderBy === property && currentOrder === "asc";
+    setCurrentOrder(isAsc ? "desc" : "asc");
+    setCurrentOrderBy(property);
   };
   const handleRowClick = (clickedRow: any) => {
     console.log(clickedRow);
@@ -148,48 +99,45 @@ const EnhancedTable: React.FC<EnhancedTableProps<EmploymentWithAction>> = ({
 
   const visibleRows = React.useMemo(
     () =>
-      stableSort(rows, getComparator(order, orderBy)).slice(
+      stableSort(rows, getComparator(currentOrder, currentOrderBy)).slice(
         page * rowsPerPage,
         page * rowsPerPage + rowsPerPage
       ),
-    [order, orderBy, page, rowsPerPage]
+    [currentOrder, currentOrderBy, page, rowsPerPage, rows]
   );
 
   return (
     <Container sx={{ width: "100%", padding: 2 }}>
-      <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
-        <Badge
-          badgeContent={notifiactionCount}
-          sx={{
-            color: (theme) => {
-              return theme.palette.text.primary;
-            },
-          }}
-        >
-          <NotificationsIcon />
-        </Badge>
-      </Box>
       <Paper sx={{ width: "100%", mb: 2 }}>
-        <EnhancedTableToolbar />
+        <EnhancedTableToolbar name={tableName} />
         <TableContainer>
           <Table
             sx={{ minWidth: 750, textAlign: "start" }}
             aria-labelledby="tableTitle"
             size={dense ? "small" : "medium"}
           >
-            <EnhancedTableHead<EmploymentWithAction>
-              order={order}
-              orderBy={orderBy}
-              rowCount={rows.length}
-              headCells={headCells}
+            <EnhancedTableHead<T>
+              order={currentOrder}
+              orderBy={currentOrderBy}
+              rowCount={rowCount}
+              headCells={
+                actionCell
+                  ? [
+                      ...headCells,
+                      {
+                        disablePadding: false,
+                        id: "action",
+                        label: "Action",
+                        numeric: false,
+                      },
+                    ]
+                  : headCells
+              }
               onRequestSort={handleRequestSort}
             />
 
             <TableBody>
-              {visibleRows.map((row, index) => {
-                const labelId = `enhanced-table-checkbox-${index}`;
-                console.log(labelId);
-
+              {visibleRows.map((row) => {
                 return (
                   <TableRow
                     hover
@@ -200,33 +148,15 @@ const EnhancedTable: React.FC<EnhancedTableProps<EmploymentWithAction>> = ({
                       cursor: "default",
                     }}
                   >
-                    {/* <TableCell padding="checkbox" /> */}
-                    <TableCell
-                      component="th"
-                      id={labelId}
-                      scope="row"
-                      onClick={() => handleRowClick(row)}
-                    >
-                      <Box
-                        sx={{ display: "flex", alignItems: "center", gap: 1 }}
-                      >
-                        <Avatar />
-                        {row?.name}
-                      </Box>
-                    </TableCell>
-                    <TableCell align="left">{row?.position}</TableCell>
-                    <TableCell align="left">{row?.privilege}</TableCell>
-                    <TableCell align="left">{row?.status}</TableCell>
-                    <TableCell align="left">
-                      <CP.Styled.Div>
-                        <CP.Button onClick={() => handleAcceptEmployee(row.id)}>
-                          Accept
-                        </CP.Button>
-                        <CP.Button variant="contained" color="accent">
-                          Reject
-                        </CP.Button>
-                      </CP.Styled.Div>
-                    </TableCell>
+                    {headCells.map((cell) => (
+                      <TableCell key={cell.id} align="left">
+                        {row[cell.id]}
+                      </TableCell>
+                    ))}
+
+                    {actionCell && (
+                      <TableCell align="left">{actionCell(row)}</TableCell>
+                    )}
                   </TableRow>
                 );
               })}
@@ -236,7 +166,7 @@ const EnhancedTable: React.FC<EnhancedTableProps<EmploymentWithAction>> = ({
                     height: (dense ? 32 : 54) * emptyRows,
                   }}
                 >
-                  <TableCell colSpan={6} />
+                  <TableCell colSpan={headCells.length} />
                 </TableRow>
               )}
             </TableBody>
@@ -245,7 +175,7 @@ const EnhancedTable: React.FC<EnhancedTableProps<EmploymentWithAction>> = ({
         <TablePagination
           rowsPerPageOptions={[5, 10, 25]}
           component="div"
-          count={rows.length}
+          count={rowCount}
           rowsPerPage={rowsPerPage}
           page={page}
           onPageChange={handleChangePage}
@@ -259,38 +189,6 @@ const EnhancedTable: React.FC<EnhancedTableProps<EmploymentWithAction>> = ({
       />
     </Container>
   );
-};
-export default EnhancedTable;
+}
 
-const headCells: HeadCell<EmploymentWithAction>[] = [
-  {
-    id: "name",
-    numeric: false,
-    disablePadding: false,
-    label: "Emplyee Information",
-  },
-  {
-    id: "position",
-    numeric: false,
-    disablePadding: false,
-    label: "Position",
-  },
-  {
-    id: "privilege",
-    numeric: false,
-    disablePadding: false,
-    label: "Priviledges",
-  },
-  {
-    id: "status",
-    numeric: false,
-    disablePadding: false,
-    label: "Status",
-  },
-  {
-    id: "action",
-    numeric: false,
-    disablePadding: false,
-    label: "Actions",
-  },
-];
+export default EnhancedTable;
