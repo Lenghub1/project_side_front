731082bfe0b29aea649f4d39dbe19e0d9effbb13
Nan <sreynan.soeun@anbschool.org>
2024-03-14 14:56:55 +0700

    refactor: refactor the EmployeeTable page using the reusable table component

--- src/pages/employee/EmployeeTable.tsx
+++ src/pages/employee/EmployeeTable.tsx
@@ -1,206 +1,65 @@
-import * as React from "react";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TablePagination from "@mui/material/TablePagination";
-import TableRow from "@mui/material/TableRow";
-import TableSortLabel from "@mui/material/TableSortLabel";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Paper from "@mui/material/Paper";
-import Checkbox from "@mui/material/Checkbox";
-import IconButton from "@mui/material/IconButton";
-import Tooltip from "@mui/material/Tooltip";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Switch from "@mui/material/Switch";
-import DeleteIcon from "@mui/icons-material/Delete";
-import FilterListIcon from "@mui/icons-material/FilterList";
-import { visuallyHidden } from "@mui/utils";
-import { alpha } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import { Avatar, Container } from "@mui/material";
+import { Container } from "@mui/material";
 import { Employement } from "@/utils/interfaces/Employment";
-interface Data extends Employement {}
+import { handleApiRequest } from "@/api";
+import { allEmployees } from "@/api/employee";
+import EnhancedTable from "@/components/table/Table";
+import { HeadCell } from "@/components/table/TableHead";
+import { useState, useEffect } from "react";
 
-function createData(data: Data): Data {
-  return data;
-}
-
-const rows = [
-  createData({
-    id: "1",
-    name: "John Doe",
-    position: "Software Engineer",
-    description: "Worked on web development projects.",
-    startDate: "2022-01-01",
-    endDate: "2023-01-01",
-    profilePicture: "john_doe.jpg",
-    status: "Active",
-    privilege: "Full Access",
-    workLocation: "Remote",
-  }),
-  createData({
-    id: "2",
-    name: "Jane Smith",
-    position: "UX Designer",
-    description: "Designed user interfaces for mobile applications.",
-    startDate: "2022-02-01",
-    endDate: "2023-02-01",
-    profilePicture: "jane_smith.jpg",
-    status: "Inactive",
-    privilege: "Limited Access",
-    workLocation: "Office",
-  }),
-  createData({
-    id: "3",
-    name: "Bob Johnson",
-    position: "Project Manager",
-    description: "Managed multiple software development projects.",
-    startDate: "2022-03-01",
-    endDate: "2023-03-01",
-    profilePicture: "bob_johnson.jpg",
-    status: "Active",
-    privilege: "Full Access",
-    workLocation: "Office",
-  }),
-  createData({
-    id: "4",
-    name: "Emily Davis",
-    position: "Data Analyst",
-    description: "Analyzed and interpreted data for business insights.",
-    startDate: "2022-04-01",
-    endDate: "2023-04-01",
-    profilePicture: "emily_davis.jpg",
-    status: "Active",
-    privilege: "Limited Access",
-    workLocation: "Remote",
-  }),
-  createData({
-    id: "5",
-    name: "Alex Turner",
-    position: "Frontend Developer",
-    description: "Developed user interfaces for web applications.",
-    startDate: "2022-05-01",
-    endDate: "2023-05-01",
-    profilePicture: "alex_turner.jpg",
-    status: "Active",
-    privilege: "Full Access",
-    workLocation: "Office",
-  }),
-  createData({
-    id: "6",
-    name: "Mia Rodriguez",
-    position: "Marketing Specialist",
-    description: "Executed marketing strategies for product promotion.",
-    startDate: "2022-06-01",
-    endDate: "2023-06-01",
-    profilePicture: "mia_rodriguez.jpg",
-    status: "Inactive",
-    privilege: "Limited Access",
-    workLocation: "Remote",
-  }),
-  createData({
-    id: "7",
-    name: "Chris Evans",
-    position: "Backend Developer",
-    description: "Implemented server-side logic for web applications.",
-    startDate: "2022-07-01",
-    endDate: "2023-07-01",
-    profilePicture: "chris_evans.jpg",
-    status: "Active",
-    privilege: "Full Access",
-    workLocation: "Office",
-  }),
-  createData({
-    id: "8",
-    name: "Olivia Moore",
-    position: "HR Coordinator",
-    description:
-      "Coordinated human resources activities within the organization.",
-    startDate: "2022-08-01",
-    endDate: "2023-08-01",
-    profilePicture: "olivia_moore.jpg",
-    status: "Active",
-    privilege: "Limited Access",
-    workLocation: "Office",
-  }),
-  createData({
-    id: "9",
-    name: "Ryan Carter",
-    position: "Financial Analyst",
-    description: "Conducted financial analysis for budget planning.",
-    startDate: "2022-09-01",
-    endDate: "2023-09-01",
-    profilePicture: "ryan_carter.jpg",
-    status: "Inactive",
-    privilege: "Full Access",
-    workLocation: "Remote",
-  }),
-  createData({
-    id: "10",
-    name: "Sophia Adams",
-    position: "Customer Support Representative",
-    description: "Provided assistance to customers with product inquiries.",
-    startDate: "2022-10-01",
-    endDate: "2023-10-01",
-    profilePicture: "sophia_adams.jpg",
-    status: "Active",
-    privilege: "Limited Access",
-    workLocation: "Remote",
-  }),
-];
-
-function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
-
-type Order = "asc" | "desc";
-
-function getComparator<Key extends keyof any>(
-  order: Order,
-  orderBy: Key
-): (a: { [key in Key]: string }, b: { [key in Key]: string }) => number {
-  return order === "desc"
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-// Since 2020 all major browsers ensure sort stability with Array.prototype.sort().
-// stableSort() brings sort stability to non-modern browsers (notably IE11). If you
-// only support modern browsers you can replace stableSort(exampleArray, exampleComparator)
-// with exampleArray.slice().sort(exampleComparator)
-function stableSort<T>(array: T[], comparator: (a: T, b: T) => number) {
-  const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) {
-      return order;
+const EmployeeTable = () => {
+  const [data, setData] = useState<any>([]);
+  const [error, setError] = useState<any>([]);
+  const fetchData = async () => {
+    const [response, error] = await handleApiRequest(() => allEmployees());
+    if (response) {
+      console.log(response);
+      setData(response);
+      setError(undefined);
+    } else {
+      console.log(error);
+      setError(error);
     }
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map((el) => el[0]);
-}
+  };
 
-interface HeadCell {
-  disablePadding: boolean;
-  id: keyof Data;
-  label: string;
-  numeric: boolean;
-}
+  useEffect(() => {
+    fetchData();
+  }, []);
+  if (error) {
+    if (error.response?.status === 404) {
+      return (
+        <Container sx={{ color: (theme) => theme.palette.text.primary }}>
+          <h1>There's no pending request</h1>
+        </Container>
+      );
+    } else {
+      return (
+        <Container sx={{ color: (theme) => theme.palette.text.primary }}>
+          <h1>Something went wrong</h1>
+        </Container>
+      );
+    }
+  }
+  return (
+    <Container>
+      <EnhancedTable<Employement>
+        orderBy="name"
+        order="asc"
+        headCells={headCells}
+        rows={data}
+        rowCount={data?.length}
+        tableName="Employee"
+      />
+    </Container>
+  );
+};
 
-const headCells: readonly HeadCell[] = [
+export default EmployeeTable;
+
+const headCells: HeadCell<Employement>[] = [
   {
     id: "name",
     numeric: false,
-    disablePadding: true,
+    disablePadding: false,
     label: "Emplyee Information",
   },
   {
@@ -210,10 +69,10 @@ const headCells: readonly HeadCell[] = [
     label: "Position",
   },
   {
-    id: "workLocation",
+    id: "privilege",
     numeric: false,
     disablePadding: false,
-    label: "Working Loaction",
+    label: "privilege",
   },
   {
     id: "status",
@@ -222,294 +81,3 @@ const headCells: readonly HeadCell[] = [
     label: "Status",
   },
 ];
-
-interface EnhancedTableProps {
-  numSelected: number;
-  onRequestSort: (
-    event: React.MouseEvent<unknown>,
-    property: keyof Data
-  ) => void;
-  onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  order: Order;
-  orderBy: string;
-  rowCount: number;
-}
-
-function EnhancedTableHead(props: EnhancedTableProps) {
-  const {
-    onSelectAllClick,
-    order,
-    orderBy,
-    numSelected,
-    rowCount,
-    onRequestSort,
-  } = props;
-  const createSortHandler =
-    (property: keyof Data) => (event: React.MouseEvent<unknown>) => {
-      onRequestSort(event, property);
-    };
-
-  return (
-    <TableHead>
-      <TableRow>
-        <TableCell padding="checkbox">
-          <Checkbox
-            color="primary"
-            indeterminate={numSelected > 0 && numSelected < rowCount}
-            checked={rowCount > 0 && numSelected === rowCount}
-            onChange={onSelectAllClick}
-            inputProps={{
-              "aria-label": "select all desserts",
-            }}
-          />
-        </TableCell>
-        {headCells.map((headCell) => (
-          <TableCell
-            key={headCell.id}
-            align={headCell.numeric ? "right" : "left"}
-            padding={headCell.disablePadding ? "none" : "normal"}
-            sortDirection={orderBy === headCell.id ? order : false}
-          >
-            <TableSortLabel
-              active={orderBy === headCell.id}
-              direction={orderBy === headCell.id ? order : "asc"}
-              onClick={createSortHandler(headCell.id)}
-            >
-              {headCell.label}
-              {orderBy === headCell.id ? (
-                <Box component="span" sx={visuallyHidden}>
-                  {order === "desc" ? "sorted descending" : "sorted ascending"}
-                </Box>
-              ) : null}
-            </TableSortLabel>
-          </TableCell>
-        ))}
-      </TableRow>
-    </TableHead>
-  );
-}
-
-interface EnhancedTableToolbarProps {
-  numSelected: number;
-}
-
-function EnhancedTableToolbar(props: EnhancedTableToolbarProps) {
-  const { numSelected } = props;
-
-  return (
-    <Toolbar
-      sx={{
-        pl: { sm: 2 },
-        pr: { xs: 1, sm: 1 },
-        ...(numSelected > 0 && {
-          bgcolor: (theme) =>
-            alpha(
-              theme.palette.primary.main,
-              theme.palette.action.activatedOpacity
-            ),
-        }),
-      }}
-    >
-      {numSelected > 0 ? (
-        <Typography
-          sx={{ flex: "1 1 100%" }}
-          color="inherit"
-          variant="subtitle1"
-          component="div"
-        >
-          {numSelected} selected
-        </Typography>
-      ) : (
-        <Typography
-          sx={{ flex: "1 1 100%" }}
-          variant="h6"
-          id="tableTitle"
-          component="div"
-        >
-          Employee
-        </Typography>
-      )}
-      {numSelected > 0 ? (
-        <Tooltip title="Delete">
-          <IconButton>
-            <DeleteIcon />
-          </IconButton>
-        </Tooltip>
-      ) : (
-        <Tooltip title="Filter list">
-          <IconButton>
-            <FilterListIcon />
-          </IconButton>
-        </Tooltip>
-      )}
-    </Toolbar>
-  );
-}
-export default function EnhancedTable() {
-  const [order, setOrder] = React.useState<Order>("asc");
-  const [orderBy, setOrderBy] = React.useState<keyof Data>("name");
-  const [selected, setSelected] = React.useState<string[]>([]);
-  const [page, setPage] = React.useState(0);
-  const [dense, setDense] = React.useState(false);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
-
-  const handleRequestSort = (
-    event: React.MouseEvent<unknown>,
-    property: keyof Data
-  ) => {
-    const isAsc = orderBy === property && order === "asc";
-    setOrder(isAsc ? "desc" : "asc");
-    setOrderBy(property);
-  };
-
-  const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.checked) {
-      const newSelected = rows.map((n) => n.id);
-      setSelected(newSelected);
-      return;
-    }
-    setSelected([]);
-  };
-
-  const handleClick = (event: React.MouseEvent<unknown>, id: string) => {
-    const selectedIndex = selected.indexOf(id);
-    let newSelected: string[] = [];
-
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, id);
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1));
-    } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(
-        selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1)
-      );
-    }
-    setSelected(newSelected);
-  };
-
-  const handleChangePage = (event: unknown, newPage: number) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
-
-  const handleChangeDense = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setDense(event.target.checked);
-  };
-
-  const isSelected = (id: string) => selected.indexOf(id) !== -1;
-
-  // Avoid a layout jump when reaching the last page with empty rows.
-  const emptyRows =
-    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
-
-  const visibleRows = React.useMemo(
-    () =>
-      stableSort(rows, getComparator(order, orderBy)).slice(
-        page * rowsPerPage,
-        page * rowsPerPage + rowsPerPage
-      ),
-    [order, orderBy, page, rowsPerPage]
-  );
-
-  return (
-    <Container sx={{ width: "100%" }}>
-      <Paper sx={{ width: "100%", mb: 2 }}>
-        <EnhancedTableToolbar numSelected={selected.length} />
-        <TableContainer>
-          <Table
-            sx={{ minWidth: 750, textAlign: "start" }}
-            aria-labelledby="tableTitle"
-            size={dense ? "small" : "medium"}
-          >
-            <EnhancedTableHead
-              numSelected={selected.length}
-              order={order}
-              orderBy={orderBy}
-              onSelectAllClick={handleSelectAllClick}
-              onRequestSort={handleRequestSort}
-              rowCount={rows.length}
-            />
-            <TableBody>
-              {visibleRows.map((row, index) => {
-                const isItemSelected = isSelected(row.id);
-                const labelId = `enhanced-table-checkbox-${index}`;
-
-                return (
-                  <TableRow
-                    hover
-                    onClick={(event) => handleClick(event, row.id)}
-                    role="checkbox"
-                    aria-checked={isItemSelected}
-                    tabIndex={-1}
-                    key={row.id}
-                    selected={isItemSelected}
-                    sx={{
-                      cursor: "pointer",
-                    }}
-                  >
-                    <TableCell padding="checkbox">
-                      <Checkbox
-                        color="primary"
-                        checked={isItemSelected}
-                        inputProps={{
-                          "aria-labelledby": labelId,
-                        }}
-                      />
-                    </TableCell>
-                    <TableCell
-                      component="th"
-                      id={labelId}
-                      scope="row"
-                      padding="none"
-                    >
-                      <Box
-                        sx={{ display: "flex", alignItems: "center", gap: 1 }}
-                      >
-                        <Avatar />
-                        {row.name}
-                      </Box>
-                    </TableCell>
-                    <TableCell align="left">{row.position}</TableCell>
-                    <TableCell align="left">{row.workLocation}</TableCell>
-                    <TableCell align="left">{row.status}</TableCell>
-                  </TableRow>
-                );
-              })}
-              {emptyRows > 0 && (
-                <TableRow
-                  style={{
-                    height: (dense ? 33 : 53) * emptyRows,
-                  }}
-                >
-                  <TableCell colSpan={6} />
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <TablePagination
-          rowsPerPageOptions={[5, 10, 25]}
-          component="div"
-          count={rows.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onPageChange={handleChangePage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        />
-      </Paper>
-      <FormControlLabel
-        control={<Switch checked={dense} onChange={handleChangeDense} />}
-        label="Dense padding"
-      />
-    </Container>
-  );
-}
