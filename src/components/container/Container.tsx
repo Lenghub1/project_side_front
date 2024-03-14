e34bb48f89487813e3aced2806655556fc53aebf
Nan <sreynan.soeun@anbschool.org>
2024-03-14 17:12:09 +0700

    add: create reusable container component

---
+++ src/components/container/Container.tsx
@@ -0,0 +1,27 @@
+import MuiContainer, {
+  ContainerProps as MuiCatainerProps,
+} from "@mui/material/Container";
+import { ReactNode } from "react";
+
+export interface ContainerProps extends MuiCatainerProps {
+  children: ReactNode;
+  maxwidth?: number;
+  isfixed?: boolean;
+}
+
+const Container = ({ children, ...props }: ContainerProps) => {
+  return (
+    <MuiContainer
+      sx={{
+        color: (theme) => {
+          return theme.palette.text.primary;
+        },
+      }}
+      {...props}
+    >
+      {children}
+    </MuiContainer>
+  );
+};
+
+export default Container;
