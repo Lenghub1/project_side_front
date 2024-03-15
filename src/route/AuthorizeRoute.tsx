5fe99b15b19ff2bbfe9d443b771407fa940fbdd4
Nan <sreynan.soeun@anbschool.org>
2024-03-15 22:42:32 +0700

    add: implement authorize-route component to restrict access

--- src/route/AuthorizeRoute.tsx
+++ src/route/AuthorizeRoute.tsx
@@ -1,33 +1,32 @@
 import React from "react";
-import { useNavigate, Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { userState } from "@/store/userStore";
 import useFetch from "@/hooks/useFetch";
 import { getUserEmployments } from "@/api/employee";
-interface AuthorizedRouteProps {
+interface ProtectedRouteProps {
   children: React.ReactNode;
   adminOnly?: boolean;
 }
 
-const AuthorizedRoute: React.FC<AuthorizedRouteProps> = ({ adminOnly }) => {
-  const navigate = useNavigate();
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ adminOnly }) => {
   const user = useRecoilValue(userState);
   console.log(user);
-  if (!user) {
-    // Redirect to login if user is not authenticated
-    navigate("/login");
-  }
   const { data, error } = useFetch(getUserEmployments);
 
   if (error) {
     console.log(error);
   }
+  console.log(data);
 
   const isAuthorized = data?.filter(
     (em: any) => em.role === "admin" || em.role === "super-admin"
   );
 
-  if (adminOnly && isAuthorized) {
+  if (!user) {
+    // Redirect to login if user is not authenticated
+    return <Navigate to="/test-login" />;
+  } else if (adminOnly && isAuthorized) {
     // Redirect to unauthorized page if user is not admin or superadmin
     return <Navigate to="/unauthorized" />;
   } else {
@@ -35,4 +34,4 @@ const AuthorizedRoute: React.FC<AuthorizedRouteProps> = ({ adminOnly }) => {
   }
 };
 
-export default AuthorizedRoute;
+export default ProtectedRoute;
