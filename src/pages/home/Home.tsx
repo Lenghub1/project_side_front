dc109757ed8daec01d034472763a7eda11552f58
Nan <sreynan.soeun@anbschool.org>
2024-03-15 22:48:43 +0700

    change: organize the employer related pages in the template properly

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,17 +1,19 @@
 import { useRecoilValue } from "recoil";
+import { useRecoilValue } from "recoil";
 import CP from "@/components";
 import AfterLoginTemplate from "@/components/template/AfterLogin";
-import { Outlet } from "react-router-dom";
-import Store from "@/store";
+import Organization from "../organization/Organization";
 
 const HomePage = () => {
+  const user = useRecoilValue(Store.User.userState);
+  console.log(user);
   const user = useRecoilValue(Store.User.userState);
   console.log(user);
 
   return (
     <AfterLoginTemplate>
       <CP.Styled.Wrapper>
-        <Outlet />
+        <Organization />
       </CP.Styled.Wrapper>
     </AfterLoginTemplate>
   );
