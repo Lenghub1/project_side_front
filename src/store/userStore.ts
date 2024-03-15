f7ae2031d24b000821c710d82a1300f492378306
Nan <sreynan.soeun@anbschool.org>
2024-03-15 22:56:43 +0700

    test: test api of employee with authorization header

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -3,13 +3,6 @@ import { recoilPersist } from "recoil-persist";
 
 const { persistAtom } = recoilPersist();
 
-// select campus
-export const campusState = atom<string | null>({
-  key: `campusState`,
-  default: null,
-  effects_UNSTABLE: [persistAtom],
-});
-
 // login user info
 export const loginUserState = atom<string | null>({
   key: `loginUserState`,
