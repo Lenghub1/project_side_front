5ed6badab46b05cf63046603c7c94239a4f6f51d
Kimsang Mok <kimsang.mok@anbschool.org>
2024-03-16 09:49:22 +0700

    refactor: implement asynchronous custom hooks

--- src/hooks/usePersistLogin.ts
+++ src/hooks/usePersistLogin.ts
@@ -1,19 +1,29 @@
 import { useEffect, useRef } from "react";
-import { useRecoilState } from "recoil";
+import { useRecoilState, useSetRecoilState } from "recoil";
 import { accessTokenState, persistLoginState } from "@/store/userStore";
 import { refreshAccessToken } from "@/utils/authUtils";
 
 function usePersistLogin() {
   const [accessToken, setAccessToken] = useRecoilState(accessTokenState);
-  const [persistLogin, setPersistLogin] = useRecoilState(persistLoginState);
+  const setPersistLogin = useSetRecoilState(persistLoginState);
+  const effectRan = useRef(false);
 
   useEffect(() => {
-    const verifyRefreshToken = async () => {
-      await refreshAccessToken(setAccessToken);
-      setPersistLogin(true);
-    };
+    if (
+      effectRan.current === true ||
+      import.meta.env.VITE_NODE_ENV !== "development"
+    ) {
+      const verifyRefreshToken = async () => {
+        await refreshAccessToken(setAccessToken);
+        setPersistLogin(true);
+      };
 
-    if (!accessToken) verifyRefreshToken();
+      if (!accessToken) verifyRefreshToken();
+    }
+    return () => {
+      effectRan.current = true;
+      return;
+    };
   }, [accessToken, setAccessToken]);
 }
 
