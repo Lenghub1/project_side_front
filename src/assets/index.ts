a86612a786b9569f53d2c7cabe0d1160920b2e97
Nan <sreynan.soeun@anbschool.org>
2024-03-18 17:24:25 +0700

    add: create central import and export for assets in the project

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,6 +2,5 @@ import err404 from "/errors/404.svg";
 import err400 from "/errors/400.svg";
 import err500 from "/errors/500.svg";
 import err401 from "/errors/401.svg";
-import something_went_wrong from "/errors/something_went_wrong.svg";
 
-export { err404, err401, err400, err500, something_went_wrong };
+export { err404, err401, err400, err500 };
