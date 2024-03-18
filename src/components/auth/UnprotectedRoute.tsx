8fd0153e213097552c730b481c61d0ee363b8701
kimsang <kimsang.mok@anbschool.org>
2024-03-18 13:21:24 +0700

    refactor: remove unnecessary import

--- src/components/auth/UnprotectedRoute.tsx
+++ src/components/auth/UnprotectedRoute.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navigate, Location, useLocation } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import useAuth from "@/hooks/useAuth";
 
 interface UnprotectedRouteProps {
@@ -12,7 +12,6 @@ const UnprotectedRoute: React.FC<UnprotectedRouteProps> = ({
   redirectPath = "/",
 }) => {
   const { isAuthenticated } = useAuth();
-  const location = useLocation();
 
   if (isAuthenticated) {
     return <Navigate to={redirectPath} replace />;
