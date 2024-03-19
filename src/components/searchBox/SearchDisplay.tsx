6787993281302ed0f0fd513cb60309fa3485fee4
Nan <sreynan.soeun@anbschool.org>
2024-03-19 20:09:35 +0700

    change: modify search components

--- src/components/searchBox/SearchDisplay.tsx
+++ src/components/searchBox/SearchDisplay.tsx
@@ -1,46 +1,46 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import CancelIcon from "@mui/icons-material/Cancel";
+import IconButton from "@mui/material/IconButton";
 import Search from "./Search";
 import CP from "..";
+import { Tooltip } from "@mui/material";
 
 const SearchDisplay = ({ data }) => {
   const [showSearch, setShowSearch] = useState(false);
   const [searchValue, setSearchValue] = useState("");
 
   const toggleSearch = () => {
-    setShowSearch(!showSearch);
-    if (!showSearch) {
-      setSearchValue("");
-    }
+    setShowSearch((previous) => !previous);
   };
 
   const handleSearchClick = () => {
-    if (searchValue.trim() !== "") {
-      console.log("Sending request to backend with search value:", searchValue);
-    }
-  };
-
-  const handleInputChange = (event) => {
-    setSearchValue(event.target.value);
+    console.log("Sending request to backend with search value:", searchValue);
   };
 
   const handleCancelClick = () => {
     setSearchValue("");
+    setShowSearch(false);
   };
 
   return (
-    <CP.Styled.Flex justify="flex-end">
+    <CP.Styled.Flex justify="flex-end" width="100%">
       {showSearch ? (
         <>
-          <CancelIcon onClick={handleCancelClick} />
           <Search data={data} />
+          <IconButton onClick={handleCancelClick}>
+            <CancelIcon />
+          </IconButton>
+          <IconButton onClick={handleSearchClick}>
+            <SearchIcon />
+          </IconButton>
         </>
       ) : (
-        <SearchIcon onClick={toggleSearch} />
-      )}
-      {showSearch && (
-        <SearchIcon onClick={handleSearchClick} style={{ marginLeft: "8px" }} />
+        <Tooltip title="search">
+          <IconButton onClick={toggleSearch}>
+            <SearchIcon />
+          </IconButton>
+        </Tooltip>
       )}
     </CP.Styled.Flex>
   );
