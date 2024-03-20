ce1a6186a7b02b0fdaee5ac8b2b79990276e476b
Nan <sreynan.soeun@anbschool.org>
2024-03-20 15:42:14 +0700

    change: change redirect path in the auth protected route

--- src/components/auth/UnprotectedRoute.tsx
+++ src/components/auth/UnprotectedRoute.tsx
@@ -17,7 +17,7 @@ const UnprotectedRoute: React.FC<UnprotectedRouteProps> = ({
 
   if (isAuthenticated) {
     if (!selected) {
-      return <Navigate to="/login/organizations" replace />;
+      return <Navigate to="/login/choose-organizations" replace />;
     }
     if (selected) {
       return <Navigate to={redirectPath} replace />;
