cc38b256bb0730fdd461a31f760b89a6d6868084
Nan <sreynan.soeun@anbschool.org>
2024-03-20 16:40:33 +0700

    change: change type of selectBranch state

--- src/store/branch.ts
+++ src/store/branch.ts
@@ -1,10 +1,10 @@
 import { atom } from "recoil";
 import { recoilPersist } from "recoil-persist";
-
+import { BranchData } from "@/utils/interfaces/Branch";
 const { persistAtom } = recoilPersist();
 
-export const selectBranch = atom<string>({
+export const selectBranch = atom<BranchData>({
   key: `selectBranch`,
-  default: "",
+  default: undefined,
   effects_UNSTABLE: [persistAtom],
 });
