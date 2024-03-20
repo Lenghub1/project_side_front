f48e771a8be16965ccfa210f111edb77bb10a93b
Nan <sreynan.soeun@anbschool.org>
2024-03-21 02:27:09 +0700

    fix: modify protected and unprotected route to fix issue when redirect

--- src/components/auth/ProtectedRoute.tsx
+++ src/components/auth/ProtectedRoute.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import useAuth from "@/hooks/useAuth";
+import { useRecoilValue } from "recoil";
+import { selectedOrganization } from "@/store/userStore";
 interface ProtectedRouteProps {
   element: any;
   allowedRoles?: string[];
@@ -10,24 +12,26 @@ const ProtectedRoute: React.FC<ProtectedRouteProps> = ({
   element,
   allowedRoles,
 }) => {
-  const { isAuthenticated, userRole, selected } = useAuth();
-  console.log(selected);
+  const navigate = useNavigate();
+  const { isAuthenticated, userRole } = useAuth();
+  const [selected] = useRecoilValue(selectedOrganization);
+  console.log(selected, isAuthenticated);
 
   if (!isAuthenticated) {
-    // redirect to login if not authenticated
-    return <Navigate to="/login" replace />;
+    navigate("/login");
+  } else {
+    if (!selected) {
+      navigate("/login/choose-organization");
+    }
   }
-  if (!selected) {
-    // Redirect to choose organization page
-    return <Navigate to="/login/choose-organization" replace />;
-  }
+
   if (allowedRoles && allowedRoles.length > 0) {
     // check if user has required roles
     const hasRequiredRole = allowedRoles.includes(userRole);
 
     if (!hasRequiredRole) {
       // redirect to unauthorized page if user doesn't have required role
-      return <Navigate to="/unauthorized" replace />;
+      navigate("/unauthorized");
     }
   }
 
--- src/components/auth/UnprotectedRoute.tsx
+++ src/components/auth/UnprotectedRoute.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import useAuth from "@/hooks/useAuth";
+
 interface UnprotectedRouteProps {
   element: any;
   redirectPath?: string;
@@ -8,18 +9,13 @@ interface UnprotectedRouteProps {
 
 const UnprotectedRoute: React.FC<UnprotectedRouteProps> = ({
   element,
-  redirectPath = "/organization",
+  redirectPath,
 }) => {
+  const navigate = useNavigate();
   const { isAuthenticated } = useAuth();
-  const selected = useRecoilValue(selectOrganization);
 
-  if (isAuthenticated) {
-    if (!selected) {
-      return <Navigate to="/login/choose-organization" replace />;
-    }
-    if (selected) {
-      return <Navigate to={redirectPath} replace />;
-    }
+  if (isAuthenticated && redirectPath) {
+    navigate(redirectPath);
   }
 
   return element;
