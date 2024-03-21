7a88c6da721b64a432b112cd98f32971e8a9e60b
Nan <sreynan.soeun@anbschool.org>
2024-03-21 16:28:47 +0700

    add: start working on the basic information component

--- src/pages/basicInformation/Information.tsx
+++ src/pages/basicInformation/Information.tsx
@@ -1,26 +1,34 @@
 import CP from "@/components";
-export interface InformationProvider {
-  userInfo: {
-    direction: string;
-    firstName: string;
-    lastName: string;
-  };
-  contact: {
-    email: string;
-    phone: string;
-    direction: string;
-  };
-  comapnyName: string;
-  gender: string;
-  birtdate: Date;
-}
+import { InformationProvider } from "@/utils/interfaces/information";
+import { Data } from "@/utils/interfaces/information";
 
-const InformationForm = () => {
+const InformationForm = ({
+  data,
+  direction,
+  fullwidth,
+}: InformationProvider) => {
+  const renderInputs = (data: { [key: string]: string | Data }) => {
+    return Object.entries(data).map(([key, value]) => {
+      if (typeof value === "string") {
+        return <CP.Input key={key} value={value} />;
+      } else {
+        return (
+          <CP.Styled.Flex>
+            <CP.Input />
+            {renderInputs(value)}
+          </CP.Styled.Flex>
+        );
+      }
+    });
+  };
+
   return (
     <CP.Container>
-      <CP.Input />
-      <CP.Input />
+      <CP.Styled.Flex direction="column" padding="0 0 2rem">
+        {renderInputs(data)}
+      </CP.Styled.Flex>
     </CP.Container>
   );
 };
+
 export default InformationForm;
