df24cc1d6e93f016dd2e031a64c44e7dc9444f61
Nan <sreynan.soeun@anbschool.org>
2024-03-25 17:30:48 +0700

    change: change the filter component ui design

--- src/components/table/filter/Filter.tsx
+++ src/components/table/filter/Filter.tsx
@@ -1,49 +1,131 @@
-import { Chip, FormControl, InputLabel, Select } from "@mui/material";
 import { useState } from "react";
-import Tooltip from "@mui/material/Tooltip";
+import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
-import FilterListIcon from "@mui/icons-material/FilterList";
+import FilterIcon from "@mui/icons-material/FilterList";
+import CloseIcon from "@mui/icons-material/Close";
+import Chip from "@mui/material/Chip";
+import Popper, { PopperPlacementType } from "@mui/material/Popper";
+import Paper from "@mui/material/Paper";
+import Stack from "@mui/material/Stack";
+import CP from "@/components";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 
-const FilterLabel: React.FC<{ label: string }> = ({ label }) => {
-  const [selectedValue, setSelectedValue] = useState<string | undefined>("");
+interface TableFilterProps {
+  headCells: any[];
+  data: any[]; // Array of objects with keys corresponding to headCell.id
+  onFilterChange: (filterKey: string, value: string) => void;
+}
 
-  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
-    setSelectedValue(event.target.value as string);
+const TableFilter = ({ headCells, data, onFilterChange }: TableFilterProps) => {
+  const [outerOpen, setOuterOpen] = useState(false);
+  const [innerOpen, setInnerOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [placement, setPlacement] = useState<PopperPlacementType>();
+  const [selectedChip, setSelectedChip] = useState<string | null>(null);
+  const [uniqueValues, setUniqueValues] = useState<string[]>([]);
+
+  const handleOuterClick =
+    (newPlacement: PopperPlacementType) =>
+    (event: React.MouseEvent<HTMLButtonElement>) => {
+      setAnchorEl(event.currentTarget);
+      setOuterOpen((prev) => placement !== newPlacement || !prev);
+      setPlacement(newPlacement);
+      if (innerOpen) {
+        setInnerOpen(false);
+      }
+    };
+
+  const handleInnerClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setInnerOpen((prev) => !prev);
+  };
+
+  const handleChipClick = (chipId: string) => {
+    if (selectedChip === chipId) {
+      setSelectedChip(null);
+      setUniqueValues([]);
+    } else {
+      setSelectedChip(chipId);
+      const values = Array.from(
+        new Set(data.map((item) => item[chipId].toString()))
+      );
+      setUniqueValues(values);
+      setOuterOpen(false);
+      if (!innerOpen) {
+        setInnerOpen(true);
+      }
+    }
+  };
+
+  const handleCloseInnerPopper = () => {
+    setInnerOpen(false);
+    setOuterOpen(true);
   };
 
   return (
-    <FormControl fullWidth>
-      <InputLabel>{label}</InputLabel>
-      <Select
-        value={selectedValue}
-        onChange={() => handleChange}
-        renderValue={(selected) => (
-          <div>
-            {(selected as string).split(",").map((item) => (
-              <Chip key={item} label={item} />
-            ))}
-          </div>
-        )}
-      ></Select>
-    </FormControl>
+    <Box>
+      <IconButton onClick={handleOuterClick("bottom-start")}>
+        {outerOpen ? <CloseIcon /> : <FilterIcon />}
+      </IconButton>
+      {outerOpen && (
+        <Popper open={outerOpen} anchorEl={anchorEl} placement={placement}>
+          <Paper sx={{ padding: 4 }}>
+            <CP.Typography variant="body1">Filter List</CP.Typography>
+            <Stack
+              gap="16px"
+              direction="column"
+              padding="1rem 0"
+              alignItems="flex-start"
+            >
+              {headCells.map((headCell) => (
+                <Chip
+                  key={headCell.id.toString()}
+                  label={headCell.label}
+                  variant={selectedChip === headCell.id ? "filled" : "outlined"}
+                  size="small"
+                  icon={
+                    selectedChip === headCell.id ? <RemoveIcon /> : <AddIcon />
+                  }
+                  onClick={() => handleChipClick(headCell.id)}
+                />
+              ))}
+            </Stack>
+          </Paper>
+        </Popper>
+      )}
+      {innerOpen && (
+        <Popper open={innerOpen} anchorEl={anchorEl} placement={placement}>
+          <Paper sx={{ padding: 4, maxHeight: "500px", overflowY: "scroll" }}>
+            <CP.Styled.Flex>
+              <IconButton aria-label="close" onClick={handleCloseInnerPopper}>
+                <ArrowBackIosIcon fontSize="small" />
+              </IconButton>
+              <CP.Typography variant="body1">
+                Unique Values for {selectedChip}
+              </CP.Typography>
+            </CP.Styled.Flex>
+            <Stack
+              gap="16px"
+              direction="column"
+              padding="1rem 0"
+              alignItems="flex-start"
+            >
+              {uniqueValues.map((value) => (
+                <Chip
+                  key={value}
+                  label={value}
+                  variant="outlined"
+                  size="small"
+                  onClick={() => onFilterChange(selectedChip!, value)}
+                />
+              ))}
+            </Stack>
+          </Paper>
+        </Popper>
+      )}
+    </Box>
   );
 };
 
-const FilterButton = ({}) => {
-  const [open, setOpen] = useState(false);
-  const handleToggleFilter = () => {
-    setOpen((prev) => !prev);
-  };
-  return (
-    <>
-      <Tooltip title="Filter">
-        <IconButton onClick={() => handleToggleFilter}>
-          <FilterListIcon />
-        </IconButton>
-      </Tooltip>
-      {open && <FilterLabel label="Filter" />}
-    </>
-  );
-};
-
-export default FilterButton;
+export default TableFilter;
