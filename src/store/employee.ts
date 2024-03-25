81582d36599c908179dab8f531610a8b7a7825eb
Nan <sreynan.soeun@anbschool.org>
2024-03-26 00:52:04 +0700

    change: change logic of handling data in the Filter component

--- src/store/employee.ts
+++ src/store/employee.ts
@@ -1,5 +1,8 @@
 import { atom } from "recoil";
 import { Employement } from "@/utils/interfaces/Employment";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
 
 export const allEmployees = atom<Employement[] | undefined>({
   key: "allEmployees",
@@ -9,4 +12,26 @@ export const allEmployees = atom<Employement[] | undefined>({
 export const employeeInfo = atom<Employement | undefined>({
   key: "employeeInfo",
   default: undefined,
+});
+
+export const employeeFeatureData = atom<Employement[] | undefined>({
+  key: "employeeFeatureData",
+  default: undefined,
+});
+
+export const employees = atom<Employement[]>({
+  key: `employees`,
+
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const selectMembers = atom<string[] | null>({
+  key: `selectMembers`,
+  default: [],
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const filteredDataState = atom({
+  key: "filteredDataState",
+  default: [],
 });
