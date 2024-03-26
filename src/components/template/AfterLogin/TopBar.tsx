3e55773d661dd997b173f607ac639f77ab80775c
Teb Yuma <yuma.teb@anbschool.org>
2024-03-26 14:03:40 +0700

    fix: redirecting page from oauth with account type

--- src/components/template/AfterLogin/TopBar.tsx
+++ src/components/template/AfterLogin/TopBar.tsx
@@ -1,12 +1,6 @@
 import { AppBar, Toolbar, Typography } from "@mui/material";
 
 const TopBar = () => {
-  const [loginUser, setLoginUser] = useRecoilState(Store.User.loginUserState);
-
-  const logout = () => {
-    setLoginUser(null);
-  };
-
   return (
     <AppBar position="sticky" sx={{ backgroundColor: "white", color: "black" }}>
       <Toolbar>
