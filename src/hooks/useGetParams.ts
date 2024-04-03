996319ff96e5a452338225fd4761a69f5bb009a9
Teb Yuma <yuma.teb@anbschool.org>
2024-04-03 13:40:59 +0700

    fix: remove comments from code

--- src/hooks/useGetParams.ts
+++ src/hooks/useGetParams.ts
@@ -6,24 +6,19 @@ interface UrlParams {
   error?: string;
 }
 
-// Hook to get URL parameters
 function useUrlParams() {
   const [params, setParams] = useState<UrlParams>({});
 
   useEffect(() => {
-    // Function to parse hash parameters from URL
     const parseHashParams = (hash: string): UrlParams => {
-      // Check if the hash contains '?'
       const queryStringStart = hash.indexOf("?");
       if (queryStringStart === -1) {
         return {};
       }
-      // Extract the query string from the hash and parse it
       const queryString = hash.substring(queryStringStart + 1);
       return Object.fromEntries(new URLSearchParams(queryString).entries());
     };
 
-    // Parse the URL parameters and set them to state
     const hashParams = parseHashParams(window.location.hash);
     setParams(hashParams);
   }, []);
