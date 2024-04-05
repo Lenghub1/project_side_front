ff4d73ce4e39285c6945bde05d10d8e39b832d70
Turleng <turleng@Turlengs-Mac-mini.local>
2024-04-05 16:05:53 +0700

    fix: delay fetch data to 1000

--- src/pages/chooseOrganization/screenJoinOrganization.tsx
+++ src/pages/chooseOrganization/screenJoinOrganization.tsx
@@ -46,7 +46,7 @@ const ScreenJoinOrganization = () => {
 
     const timeout = setTimeout(() => {
       fetchData();
-    }, 2000);
+    }, 1000);
 
     return () => clearTimeout(timeout);
   }, [user.id]);
